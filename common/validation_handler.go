994dc26aa14e7ee48b5b16466d177f27950966a0
Enrico Lorenzo Rodriguez <elorenzo.rodz@gmail.com>
2025-07-20 15:58:12 +0800

    refactor error format into string

--- common/validation_handler.go
+++ common/validation_handler.go
@@ -47,33 +47,33 @@ func ValidateJWTAndGetEmailClaim(signedToken string) (string, error) {
 	publicBytes, publicKeyError := os.ReadFile("public.pem")
 
 	if publicKeyError != nil {
-		fmt.Printf("Read public key error: %v", publicKeyError)
+		fmt.Printf("Read public key error: %s", publicKeyError)
 
-		return "", fmt.Errorf("read public key error: %v", publicKeyError)
+		return "", fmt.Errorf("read public key error: %s", publicKeyError)
 	}
 
 	publicKey, publicKeyParseError := jwt.ParseECPublicKeyFromPEM(publicBytes)
 
 	if publicKeyParseError != nil {
-		fmt.Printf("Parse public key error: %v", publicKeyParseError)
+		fmt.Printf("Parse public key error: %s", publicKeyParseError)
 
-		return "", fmt.Errorf("parse public key error: %v", publicKeyParseError)
+		return "", fmt.Errorf("parse public key error: %s", publicKeyParseError)
 	}
 
 	parsedToken, parsedTokenError := jwt.Parse(signedToken, func(token *jwt.Token) (interface{}, error) {
 		_, ok := token.Method.(*jwt.SigningMethodECDSA)
 		
 		if !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %s", token.Header["alg"])
 		}
 
 		return publicKey, nil
 	})
 
 	if parsedTokenError != nil {
-		fmt.Printf("Token parse error: %v", parsedTokenError)
+		fmt.Printf("Token parse error: %s", parsedTokenError)
 
-		return "", fmt.Errorf("token parse error: %v", parsedTokenError)
+		return "", fmt.Errorf("token parse error: %s", parsedTokenError)
 	}
 
 	if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
